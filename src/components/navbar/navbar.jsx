4d98a8510ac754788e3f8c439b9187ed539d1734
nithin <nithinsukumar200@gmail.com>
2025-07-09 23:26:53 +0530

    food displayed

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { assets } from '../../assets/assets'
 
 
 const Navbar = () => {
+
+  const [menu,setMenu]=useState("home")
   return (
     <div className='navbar'>
       <img src={assets.logo} alt='' className='logo'/>
       <ul className='navbar-menu'>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-app</li>
-        <li>COntact Us</li>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>Mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
       </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon}/>
+        <div className="navbar-search-icon">
+             <img src={assets.basket_icon} alt=''/>
+             <div className="dot">
+
+        </div>
+        
+        </div>
+        <button>sign in</button>
+        
+        
+      </div>
+      
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
 import './index.css'
-createRoot(document.getElementById('root')).render(
+import StoreContextProvider from './StoreContext/StoreContext.jsx'
 
-    <App />
+
+createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<StoreContextProvider>
+<App />
+</StoreContextProvider>
+</BrowserRouter>
+
 
 )
